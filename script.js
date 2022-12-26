f90f20db6d47013f92c916b53efe05455ca98f9f
mtojong246 <120267213+mtojong246@users.noreply.github.com>
2022-12-26 16:19:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var btn = document.querySelector("button");
+
+window.onload = function start() {
+	setGradient();
+};
+
+
+function randomize() {
+	var letters = "0123456789ABCDEF";
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[(Math.floor(Math.random() * 16))]
+	};
+	return color;
+}
+
+
+btn.addEventListener("click", function() {
+	body.style.background = "linear-gradient(to right, " + randomize() + ", " + randomize() + ")";
+	css.textContent = body.style.background + ";";
+});
+
+
+function setGradient() {
+	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+	css.textContent = body.style.background + ";";
+};
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+
+
